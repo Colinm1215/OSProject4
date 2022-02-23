58ad914d62a4c798bd1fd8a2359f26aaf38cd7c0
Colinm1215 <colinm1215@gmail.com>
2022-02-23 17:00:54 -0500

    init

---
+++ scheduler.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+struct job {
+    int id ;
+    int length ;
+    // other meta data
+    struct job * next ;
+};
+
+struct job *job_list;
+
+// function that prints job meta data
+void print_job(struct job * j) {
+    printf("Job %d: length %d\n", j->id, j->length);
+}
+
+//Read in the jobs from the file
+struct job * read_jobs(char * filename) {
+    // open the file
+    FILE * fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Couldn't open file\n");
+        return NULL;
+    }
+    // read in the jobs
+    struct job * start = NULL;
+    struct job * curr = NULL;
+    int id = 0;
+    int runtime;
+    while (fscanf(fp, "%d", &runtime) == 1) {
+        struct job * myJOB = malloc(sizeof(struct job));
+        myJOB->id = id;
+        myJOB->length = runtime;
+        myJOB->next = NULL;
+        if (start == NULL) {
+            start = myJOB;
+        } 
+        else {
+            curr->next = myJOB;
+        }
+        curr = myJOB;
+        id++;
+    }
+    fclose(fp);
+    return start;
+}
+
+void print_job_order(void){
+    struct job * current_job = job_list;
+    printf("printing job order\n");
+    while(current_job != NULL){
+        printf("job %d will run for: %d\n", current_job->id, current_job->length);
+        current_job = current_job->next;
+    }
+    printf("\n");
+
+}
+
+// Function that executes jobs first in first out
+void execute_jobs_fifo(struct job * job_list) {
+    struct job * curr = job_list;
+    print_job_order();
+}
+
+// Function moves job to end of list based on ID
+void move_to_end(int number_of_jobs, int id) {
+    struct job * current_job = job_list;
+    struct job * prev_job = NULL;
+    while (current_job != NULL) {
+        if (current_job->id == id) {
+
+            //If we are the first element in the list
+            if (prev_job == NULL) {
+                job_list = current_job->next;
+            }
+            //If we are somewhere in the middle of the list 
+            else {
+                prev_job->next = current_job->next;
+            }
+            //Move the job to the end of the list
+            struct job * tmp = job_list;
+            //current_job->next = NULL;
+            
+            for(int i = 0; i < number_of_jobs - 2; i++){
+                tmp = tmp->next;
+            }
+            current_job->next = tmp->next;
+            tmp->next = current_job;
+            return;
+        }
+        prev_job = current_job;
+        current_job = current_job->next;
+    }
+    
+}
+
+
+
+// Function that executes jobs in order of length
+int get_index_of_largest(int my_size) {
+    // Sort the jobs by length
+    struct job * current_job = job_list;
+    struct job * sorted_list = NULL;
+    int max = current_job->length;
+    int index_of_max = current_job->id;
+
+    while(current_job->next != NULL && my_size > 0){
+        if(current_job->length > max){
+            max = current_job->length;
+            index_of_max = current_job->id;
+        }
+        current_job = current_job->next;
+        my_size--;
+    }
+    return index_of_max;
+
+}
+
+//Return the total number of jobs
+int get_total_jobs(){
+    struct job * current_job = job_list;
+    int total_jobs = 0;
+    while(current_job != NULL){
+        total_jobs++;
+        current_job = current_job->next;
+    }
+    return total_jobs;
+}
+
+
+
+void execute_jobs_sjf(struct job * job_list) {
+    struct job * current_job = job_list;
+    int starting_total_jobs = get_total_jobs();
+
+    for(int i = starting_total_jobs; i > 1; i--){
+        int index_of_largest = get_index_of_largest(i);
+        move_to_end(i, index_of_largest);
+    }
+
+    print_job_order();
+
+}
+
+int main(int argc, char **argv){
+    //Get the jobs from the file
+    job_list = read_jobs(argv[1]);
+
+    printf("FIFO\n");
+    execute_jobs_fifo(job_list);
+    printf("\n");
+    printf("SJF\n");
+    execute_jobs_sjf(job_list);
+
+
+    printf("Hello, please help me schedule!\n");
+    return 0;
+}
